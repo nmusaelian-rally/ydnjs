c34144f3d52177e535a254a28c37028660876bda
N M <nmusaelian@rallydev.com>
2014-11-05 17:02:41 -0700

    zoo4 used Object.create

--- z-k/iife/zoo/lion.js
+++ z-k/iife/zoo/lion.js
@@ -15,8 +15,9 @@
     
     function Surrogate(){};
     //Surrogate.prototype = Animal.prototype; //will cause Uncaught ReferenceError: Animal is not defined 
-    Surrogate.prototype = Zoo.Animal.prototype;
-    Lion.prototype = new Surrogate(); //set Lion prototype to Surrogatge instance
+    //Surrogate.prototype = Zoo.Animal.prototype;
+    //Lion.prototype = new Surrogate(); //set Lion prototype to Surrogatge instance
+    Lion.prototype = Object.create(Zoo.Animal.prototype);
     Lion.prototype.roar = function(){
         console.log('roar!')
     }
@@ -25,7 +26,9 @@
 /*
  Lion.prototype.__proto__ must be Animal.prototype, so we can call Animal methods on a Lion
 Constructing Lion.prototype must not involve calling the Animal constructor function
-This is done via a third class usually called Surrogate.
+This used to be done via a third class traditionally called Surrogate.
 The reason why we don't just call Lion.prototype=Animal.prototype is because we dont want that any function added to Lion to be added to Animal,
 e.g. roar() should exist on Lion only.
+
+As of ECMAScript 5 we don"t have to use Surrogate. Object.create returns new obj with its __proto__ set to its first argument.
  */
